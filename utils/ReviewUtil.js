d2a5818da00d97a66fb745ac20b8c43245b9f01c
javierwong1508@gmail.com <javierwong1508@gmail.com>
2023-11-27 03:27:16 +0800

    Accident 2

--- utils/ReviewUtil.js
+++ utils/ReviewUtil.js
@@ -3,7 +3,7 @@ const { Review } = require('../models/review');
 const fs = require('fs').promises;
 async function viewReviews(req, res) {
     try {
-        const allReviews = await readJSON('utils/reviews.json');
+        const allReviews = await readJSON('utils/review.json');
         return res.status(201).json(allReviews);
     } catch (error) {
         return res.status(500).json({ message: error.message });
