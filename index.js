9d7168472d299e987a8df805fc724fa863d48fcc
MentalPains <Johnathon93639@gmail.com>
2023-11-26 23:34:22 +0800

    Implemented the backend for add-resource

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
+
 const PORT = process.env.PORT || 5050
 var startPage = "index.html";
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
+
+const { register, login } = require('./utils/UserUtil')
+app.post('/register', register);
+app.post('/login', login);
+
+const { addResource } = require('./utils/ResourceUtil')
+app.post('/add-resource', addResource)
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/" + startPage);
 })
